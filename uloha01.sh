5b2a2275446e45770743af6c225a5ad941e2e65c
Katerina Polakova <katerina.polakova@outlook.com>
2019-03-18 19:25:18 +0100

    updatovana verze

--- uloha01.sh
+++ uloha01.sh
@@ -1,41 +1,48 @@
 #!/bin/sh
-if [test "$1" == "--typ"]; then
-if test ! -e "$2" ; then
-   echo "Cesta k souboru nenalezena"
-   exit 1
-else   
-   if test -d "$2";then
-   	echo "adresar";
-   elif test -f "$2"; then
-   	echo "soubor";
-   elif test -L "$2";then 
-     echo "Symbolicky link "
-   elif test -p "$2";then
-     echo "Pojmenovana roura"
-   elif test -s "$2";then
-     echo "Socket "
-   elif test -c "$2";then
-     echo "souborove zarizeni"
-   elif test -D "$2";then
-     echo "Dvere"
+
+if [ "$1" == "--help" ]; then
+    echo 'Pouziti: ./uloha01.sh [--typ|--help] [cesta_k_souboru]
+    --typ
+    	   a) Adresar
+	   b) Soubor
+	   c) symbolicky link
+	   d) Fifo (roura)
+	   e) Socket
+	   f] Znakove zarizeni
+	   g) Blokove zarizeni
+	   h) Dvere
+    --help
+        pomoc...vysvetli zpusob pouziti skryptu
+
+   Exit status:
+     0 uspech
+     1 chyba'
+   exit 0
+elif [ "$1" == "--typ" ]; then
+   if test ! -e "$2" ; then
+   	echo 'soubor neexistuje'
+	exit 1
    else
-     echo "Neumim urcit typ souboru "
-     exit 1
-  fi
-  exit 0
+      if test -d "$2" ; then
+      echo 'Adresar'
+      elif test -f "$2"; then
+      echo 'Soubor'
+      elif test -L "$2"; then
+      echo 'Symbolicky link'
+      elif test -p "$2"; then
+      echo 'Fifo(roura)'
+      elif test -s "$2"; then
+      echo 'Socket'
+      elif test -c "$2"; then
+      echo 'Znakove zarizeni'
+      elif test -b "$2"; then
+      echo 'Blokove zarizeni'
+      elif test -D "$2"; then
+      echo 'Dvere'
+      fi
+      exit 0
 fi
-elif ["$1" == "--help"];then
-   echo "Pouziti: uloha01.sh [--typ|--help] [cesta k souboru]"
-   echo "  --typ       Vypise, zda je dany soubor adresar,soubor" 
-   echo "              socket, dvere,souborove zarizeni, nebo symbolicky link"
-   echo "  --help	Vypise navod k pouziti skriptu"
-   echo" "
-   echo "Exit status: "
-   echo "  0 ... Vse ok"
-   echo "  1 ... Doslo k chybe"
 else
-  echo "Zapomneli jste zadat urcujici parametr pro prubeh skriptu."
-  echo "Nebo jste jej zadali nespravne"
+  echo 'chyba'
   exit 1
 fi
-exit 0
