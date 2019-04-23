52e4f4ecddbf355ca85c3abf5e06186299eb5acb
Katerina Polakova <katerina.polakova@outlook.com>
2019-04-23 13:46:02 +0200

    done

--- uloha03/my_tac.sh
+++ uloha03/my_tac.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
 
-file=/tmp/helper.txt
-cut -d ' '   /tmp/helper.txt
-
-while IFS= read -r line
+FILE=$(mktemp /tmp/help.XXXXXX)
+cat - > $FILE
+K=$( cat $FILE | wc -l )
+if [ $# -eq 2 ]; then
+  if [ "$1" == "-n" ]; then
+    A=$2
+  else
+    exit 1
+  fi
+elif [ $# -eq 0 ]; then 
+  A=1
+else 
+  exit 1
+fi
+H=$(($K % $A ))
+for (( i = $A; i <= $K ; i=i+A))
 do
-        # display line or do somthing on $line
-	echo "$line"
-done <"$file"
-
+    cat $FILE | tail -n $i | head -n $A
+done
+if [ $H -ne 0 ]; then
+   cat $FILE | tail -n $K  | head -n $H
+fi
+rm "$FILE"
